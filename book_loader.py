4f8afaf16ed4709c56ea64b86a0b683f40f2d722
Khorin Vitaly <khorin.v@gmail.com>
2021-03-17 15:11:33 +0600

    download shots

--- book_loader.py
+++ book_loader.py
@@ -3,6 +3,7 @@ import os
 from bs4 import BeautifulSoup
 import urllib3
 from pathvalidate import sanitize_filename
+import urllib.parse
 
 
 def download_txt_file(url, filename, folder='books/'):
@@ -20,7 +21,20 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def get_book_title(url):
+def download_image(url, folder='images/'):
+    response = requests.get(url, verify=False, allow_redirects=False)
+    if response.status_code != 200:
+        return
+    os.makedirs(folder, exist_ok=True)
+    url_parts = urllib.parse.urlsplit(url, scheme='', allow_fragments=True)
+    filename = os.path.basename(url_parts.path)
+    filepath = os.path.join(folder, sanitize_filename(filename))
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+    return filepath
+
+
+def parse_book_page(url):
     response = requests.get(url, verify=False)
     soup = BeautifulSoup(response.text, 'html.parser')
     content = soup.find('div', id='content')
@@ -28,27 +42,34 @@ def get_book_title(url):
         return
     title_tag = content.find('h1')
     parts_of_title = title_tag.text.split('::')
-    return parts_of_title[0].strip()
+    img_tag = content.find('div', 'bookimage').find('img')
+    book_properties = {
+        'name': parts_of_title[0].strip(),
+        'autor': parts_of_title[1].strip(),
+        'img_url': img_tag.attrs['src']
+    }
+    return book_properties
 
 
-def main():
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+def main(root_url):
     for id in range(1, 11):
-        book_url = f'http://tululu.org/b{id}/'
-        title = get_book_title(book_url)
-        if not title:
-            print(f'Title not found at {book_url} check the url!')
+        book_url = f'{root_url}/b{id}/'
+        book_properties = parse_book_page(book_url)
+        if not book_properties:
+            print(f'Cant parse {book_url} check the url!')
             continue
-        filename = f'{id}.{title}.txt'
+        book_filename = f'{id}.{book_properties["autor"]}_{book_properties["name"]}.txt'
         try:
-            download_url = f'https://tululu.org/txt.php?id={id}'
-            filepath = download_txt_file(download_url, filename)
-            print(f'The book loaded successfully {filepath}')
+            txt_download_url = f'{root_url}/txt.php?id={id}'
+            book_filepath = download_txt_file(txt_download_url, book_filename)
+            img_download_url = f'{root_url}/{book_properties["img_url"]}'
+            img_filepath = download_image(img_download_url)
+            print(f'The book, shot loaded successfully from {book_url}')
+
         except requests.HTTPError:
-            print(f'Book from {download_url} not loaded something was wrong!')
+            print(f'Book from {book_url} not loaded something was wrong!')
 
 
-# url = 'http://tululu.org/b1/'
-# get_book_properties(url)
-
-main()
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+main('https://tululu.org')
+# print(download_image('https://tululu.org/shots/9.jpg'))
