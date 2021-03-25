b041c5336cbc539f22504d2784eecdf76189bbf7
Khorin Vitaly <khorin.v@gmail.com>
2021-03-25 11:58:30 +0600

    add arguments to script

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,11 +1,11 @@
 import requests
-import os
 from bs4 import BeautifulSoup
 import urllib3
 from urllib.parse import urljoin
 from book_loader import parse_book_page, download_txt_file, download_image, check_response
 from tqdm import tqdm
 import json
+import argparse
 
 
 ROOT_URL = 'https://tululu.org'
@@ -35,26 +35,23 @@ def write_to_json_file(data, filename):
 
 def main():
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    book_counter = 0
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--start_page', help='Начальная страница', default=1)
+    parser.add_argument('--end_page', help='Конечная страница', default=11)
+    args = parser.parse_args()
     science_fiction_category_books = []
-    for i in range(1, 10):
-        if book_counter >= 100:
-            break
+    for i in tqdm(range(args.start_page, args.end_page), desc='Pages'):
         category_url = urljoin(ROOT_URL, BOOK_CATEGORY, i)
         response = requests.get(category_url, verify=False)
         soup = BeautifulSoup(response.text, 'html.parser')
         links_to_books = soup.select('.d_book .bookimage a')
-        for tag in links_to_books:
-            if book_counter >= 100:
-                break
+        for tag in tqdm(links_to_books, desc=f'Books on page({i})'):
             href = tag.attrs['href']
             book_url = urljoin(ROOT_URL, href)
             id = int(href.replace('b', '').replace('/', ''))
             book_properties = fetch_book_data(book_url, id)
             if book_properties:
                 science_fiction_category_books.append(book_properties)
-            book_counter += 1
-            print(book_url)
     write_to_json_file(science_fiction_category_books, 'books')
 
 
